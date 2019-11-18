6ac851c49154bbdb347c532421e1710a0ec34d00
ritasantos11 <37849843+ritasantos11@users.noreply.github.com>
2019-11-18 21:05:33 +0000

    Update G.java

--- Vol3/G.java
+++ Vol3/G.java
@@ -64,48 +64,22 @@ class Grafo0 {
 
 
 class G {
-    public static void ks(Grafo0 g) {
+    public static void kosaraju(Grafo0 g) {
         Stack<Integer> s = dfs(g);
         Grafo0 gt = transposto(g);
         boolean visitado[] = new boolean[g.num_vertices()+1];
 
-        int grupo=0, semgrupo=g.num_vertices(), n=0;
+        int comgrupo=0, semgrupo=g.num_vertices();
         while (!s.isEmpty()) {
             int v = s.pop();
-            
-            if (!visitado[v]) {
-               n = bfs(gt,visitado,v);
-            
-                if (n>=4) {
-                    grupo++;
-                    semgrupo -= n;
-                }
+            int n = bfs(gt, v, visitado);
+            if (n>=4) {
+                comgrupo++;
+                semgrupo -= n;
             }
         }
 
-        System.out.println(grupo + " " + semgrupo);
-    }
-
-    public static int bfs(Grafo0 gt, boolean[] visitado, int origem) {
-        Queue<Integer> q = new LinkedList<Integer>();
-        q.add(origem);
-        visitado[origem] = true;
-
-        int n=0;
-        while (!q.isEmpty()) {
-            int v = q.remove();
-            n++;
-            LinkedList<Arco> adjs = gt.adjs_no(v);
-            for (Arco a : adjs) {
-                int w = a.extremo_final();
-                if (!visitado[w]) {
-                    visitado[w]=true;
-                    q.add(w);
-                }
-            }
-        }
-
-        return n;
+        System.out.println(comgrupo + " " + semgrupo);
     }
 
     public static Grafo0 transposto(Grafo0 g) {
@@ -115,57 +89,74 @@ class G {
             LinkedList<Arco> adjs = g.adjs_no(v);
             for (Arco a : adjs) {
                 int w = a.extremo_final();
-
-                gt.insert_new_arc(w,v);
+                gt.insert_new_arc(w, v);
             }
         }
 
         return gt;
     }
 
-    public static Stack<Integer> dfs(Grafo0 g) {
-        Stack<Integer> s = new Stack<Integer>();
-        boolean visitado[] = new boolean[g.num_vertices()+1];
+    public static int bfs(Grafo0 g, int origem, boolean visitado[]) {
+        Queue<Integer> q = new LinkedList<>();
+        q.add(origem);
+        visitado[origem] = true;
         
-        for (int v=1; v<=g.num_vertices(); v++)
-            if (!visitado[v])
-                dfs_visit(g,visitado,s,v);
+        int n=0;
+        while (!q.isEmpty()) {
+            int v = q.remove();
+            n++;
+            LinkedList<Arco> adjs = g.adjs_no(v);
+            for (Arco a : adjs) {
+                int w = a.extremo_final();
+                if (!visitado[w]) {
+                    visitado[w] = true;
+                    q.add(w);
+                }
+            }
+            
+        }
 
+        return n;
+    }
+
+    public static Stack<Integer> dfs(Grafo0 g) {
+        boolean visitado[] = new boolean[g.num_vertices()+1];
+        Stack<Integer> s = new Stack<Integer>();
+        
+        for (int i=1; i<=g.num_vertices(); i++)
+            if (!visitado[i]) dfs_visit(i, g, visitado, s);
+        
         return s;
     }
 
-    public static void dfs_visit(Grafo0 g, boolean visitado[], Stack<Integer> s, int v) {
+    public static void dfs_visit(int v, Grafo0 g, boolean visitado[], Stack<Integer> s) {
         visitado[v] = true;
         LinkedList<Arco> adjs = g.adjs_no(v);
         for (Arco a : adjs) {
             int w = a.extremo_final();
-            if (!visitado[w])
-                dfs_visit(g, visitado, s, w);
+            if (!visitado[w]) dfs_visit(w, g, visitado, s);
         }
 
         s.push(v);
     }
 
-
-
     public static void main(String[] args) {
         Scanner stdin = new Scanner(System.in);
 
-        int ncasos = stdin.nextInt();
-        for (int i=1; i<=ncasos; i++){
-            int maxalunos = stdin.nextInt();
-            Grafo0 g = new Grafo0(maxalunos);
-            for (int j=0; j<maxalunos; j++) {
-                int idaluno = stdin.nextInt();
-                int nalunos = stdin.nextInt();
-                for (int k=0; k<nalunos; k++) {
-                    int aluno = stdin.nextInt();
-                    g.insert_new_arc(idaluno, aluno);
+        int n = stdin.nextInt();
+        for (int i=1; i<=n; i++) {
+            int nalunos = stdin.nextInt();
+            Grafo0 g = new Grafo0(nalunos);
+            for (int j=0; j<nalunos; j++) {
+                int idalunoA = stdin.nextInt();
+                int numalunos = stdin.nextInt();
+                for (int k=0; k<numalunos; k++) {
+                    int idalunoB = stdin.nextInt();
+                    g.insert_new_arc(idalunoA, idalunoB);
                 }
             }
-
             System.out.println("Caso #" + i);
-            ks(g);
+            kosaraju(g);
         }
     }
 }
